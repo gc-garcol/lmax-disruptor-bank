afb6823ae0ec321623fb3a9b2710755d0164aa31
Thai Van <giacatluong20121996@gmail.com>
2024-06-16 07:03:43 +0700

    feat/cluster-cluster: config balances

--- bank-cluster-app/src/main/java/gc/garcol/bankcluster/infra/LearnerConfiguration.java
+++ bank-cluster-app/src/main/java/gc/garcol/bankcluster/infra/LearnerConfiguration.java
@@ -1,5 +1,11 @@
 package gc.garcol.bankcluster.infra;
 
+import gc.garcol.bankclustercore.account.AccountRepository;
+import gc.garcol.bankclustercore.account.Balances;
+import gc.garcol.bankclustercore.offset.SnapshotRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 /**
@@ -7,5 +13,19 @@ import org.springframework.context.annotation.Profile;
  * @since 2024
  */
 @Profile("learner")
+@Configuration
+@RequiredArgsConstructor
 public class LearnerConfiguration {
+
+    private final SnapshotRepository snapshotRepository;
+    private final AccountRepository accountRepository;
+
+    @Bean
+    public Balances balances() {
+        var balances = new Balances();
+        balances.setLastedId(accountRepository.lastedId());
+        balances.setEnableChangedCapture(true);
+        return balances;
+    }
+
 }
